96a53dac979a0f485fb370145320a791a6458bf9
Andrew Hoult <mrrobottrax@gmail.com>
2026-09-11 18:11:40 -0400

    Add some tiles

---
+++ Assets/TileUpdator.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class TileUpdator : MonoBehaviour
+{
+  public Tilemap m_DungeonTilemap;
+  public Tilemap m_EntityTilemap;
+
+  public void Awake()
+  {
+    //m_DungeonTilemap.
+  }
+}
---
+++ Assets/WorldTile.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using UnityEngine.Tilemaps;
+
+[CreateAssetMenu(fileName = "WorldTile", menuName = "Scriptable Objects/WorldTile")]
+public class WorldTile : Tile
+{
+	public GridTileData m_TileData;
+}
